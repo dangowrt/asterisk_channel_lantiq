dangowrt/asterisk_channel_lantiq

commit 51288e0d35ec7699e3c3ae1830183cbb98d1d34e

remove dial after pressing hash digit

src/channels/chan_lantiq.c
@@ -1093,27 +1093,18 @@ static void lantiq_dev_event_digit(int c, char digit)
 
 			/* fall through */
 		case DIALING: 
-			if (digit == '#') {
-				if (pvt->dial_timer) {
-					ast_sched_thread_del(sched_thread, pvt->dial_timer);
-					pvt->dial_timer = 0;
-				}
+			pvt->dtmfbuf[pvt->dtmfbuf_len] = digit;
+			pvt->dtmfbuf_len++;
+			pvt->dtmfbuf[pvt->dtmfbuf_len] = '\0';
 
-				lantiq_dial(pvt);
+			/* setup autodial timer */
+			if (!pvt->dial_timer) {
+				ast_log(LOG_DEBUG, "setting new timer\n");
+				pvt->dial_timer = ast_sched_thread_add(sched_thread, 4000, lantiq_event_dial_timeout, (const void*) pvt);
 			} else {
-				pvt->dtmfbuf[pvt->dtmfbuf_len] = digit;
-				pvt->dtmfbuf_len++;
-				pvt->dtmfbuf[pvt->dtmfbuf_len] = '\0';
-
-				/* setup autodial timer */
-				if (!pvt->dial_timer) {
-					ast_log(LOG_DEBUG, "setting new timer\n");
-					pvt->dial_timer = ast_sched_thread_add(sched_thread, 4000, lantiq_event_dial_timeout, (const void*) pvt);
-				} else {
-					ast_log(LOG_DEBUG, "replacing timer\n");
-					struct sched_context *sched = ast_sched_thread_get_context(sched_thread);
-					AST_SCHED_REPLACE(pvt->dial_timer, sched, 4000, lantiq_event_dial_timeout, (const void*) pvt);
-				}
+				ast_log(LOG_DEBUG, "replacing timer\n");
+				struct sched_context *sched = ast_sched_thread_get_context(sched_thread);
+				AST_SCHED_REPLACE(pvt->dial_timer, sched, 4000, lantiq_event_dial_timeout, (const void*) pvt);
 			}
 			break;
 		default:

commit 973478e4567837047b208b77bbe97acfd8637ed7

Prevent buffer overrun while dialing a number.
Note: Merged conflicts caused by commit:
51288e0d35ec7699e3c3ae1830183cbb98d1d34e (remove dial after pressing
hash digit)

src/channels/chan_lantiq.c
@@ -122,9 +122,9 @@ static struct lantiq_pvt {
 	int port_id;                       /* Port number of this object, 0..n      */
 	int channel_state;
 	char context[AST_MAX_CONTEXT];     /* this port's dialplan context          */
-	char ext[AST_MAX_EXTENSION+1];     /* the extension this port is connecting */
+	char ext[AST_MAX_EXTENSION];       /* the extension this port is connecting */
 	int dial_timer;                    /* timer handle for autodial timeout     */
-	char dtmfbuf[AST_MAX_EXTENSION+1]; /* buffer holding dialed digits          */
+	char dtmfbuf[AST_MAX_EXTENSION];   /* buffer holding dialed digits          */
 	int dtmfbuf_len;                   /* lenght of dtmfbuf                     */
 	int rtp_timestamp;                 /* timestamp for RTP packets             */
 	int ptime;			   /* TAPI base enconder ptime		    */
@@ -1338,9 +1338,16 @@ static void lantiq_dev_event_digit(int c, char digit)
 
 			/* fall through */
 		case DIALING: 
-			pvt->dtmfbuf[pvt->dtmfbuf_len] = digit;
-			pvt->dtmfbuf_len++;
-			pvt->dtmfbuf[pvt->dtmfbuf_len] = '\0';
+			if (pvt->dtmfbuf_len < AST_MAX_EXTENSION - 1) {
+				pvt->dtmfbuf[pvt->dtmfbuf_len] = digit;
+				pvt->dtmfbuf[++pvt->dtmfbuf_len] = '\0';
+			} else {
+				/* No more room for another digit */
+				lantiq_end_dialing(c);
+				lantiq_play_tone(pvt->port_id, TAPI_TONE_LOCALE_BUSY_CODE);
+				pvt->channel_state = CALL_ENDED;
+				break;
+			}
 
 			/* setup autodial timer */
 			if (!pvt->dial_timer) {